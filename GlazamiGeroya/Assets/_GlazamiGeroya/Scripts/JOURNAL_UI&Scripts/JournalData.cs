f266a622ef3a6dd79ef25b11a7a9569e2e4bc797
Chunyai <dericsaaa@gmail.com>
2026-04-30 18:24:54 +0700

    Система заданий запасной комит

--- GlazamiGeroya/Assets/_GlazamiGeroya/Scripts/JOURNAL_UI&Scripts/JournalData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections.Generic;
-
-[CreateAssetMenu(fileName = "JournalData", menuName = "Journal/Journal")]
-public class JournalData : ScriptableObject
-{
-    public string journalTitle = "Полевой журнал";
-    public List<TaskData> tasks = new List<TaskData>();
-}
